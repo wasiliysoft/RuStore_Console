ef6f325f6f2274339c80fc06455c546a77d412b6
WasiliySoft-Home <wasiliysoft@ya.ru>
2023-10-25 22:55:49 +0500

    colored card

--- app/src/main/java/ru/wasiliysoft/rustoreconsole/ui/view/PurchaseItem.kt
+++ app/src/main/java/ru/wasiliysoft/rustoreconsole/ui/view/PurchaseItem.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.wasiliysoft.rustoreconsole.data.Purchase
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
@@ -22,9 +24,16 @@ fun PurchaseItem(
     purchase: Purchase,
     modifier: Modifier = Modifier
 ) {
+
+    val date = LocalDateTime.parse(
+        purchase.paymentInfo.paymentDate + ":00",
+        DateTimeFormatter.ISO_OFFSET_DATE_TIME
+    )
+    val cardColor = if (date.toLocalDate() == LocalDate.now()) CardDefaults.cardColors()
+    else CardDefaults.outlinedCardColors()
     Card(
-        modifier = modifier
-            .fillMaxWidth()
+        colors = cardColor,
+        modifier = modifier.fillMaxWidth()
     ) {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
@@ -39,11 +48,7 @@ fun PurchaseItem(
                 Text(text = purchase.paymentInfo.paymentId.toString())
             }
             Column(horizontalAlignment = Alignment.End) {
-                Text(text = purchase.amountCurrent.toString())
-                val date = LocalDateTime.parse(
-                    purchase.paymentInfo.paymentDate + ":00",
-                    DateTimeFormatter.ISO_OFFSET_DATE_TIME
-                )
+                Text(text = (purchase.amountCurrent / 100).toString())
                 Text(text = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)))
                 Text(text = date.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)))
             }
