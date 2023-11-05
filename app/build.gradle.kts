6e48562ff7b4c2c28b4d02b824e5c5836d544c23
WasiliySoft-Home <wasiliysoft@ya.ru>
2023-11-05 16:21:34 +0500

    v1.2.0 build 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ru.wasiliysoft.rustoreconsole"
         minSdk = 26
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
