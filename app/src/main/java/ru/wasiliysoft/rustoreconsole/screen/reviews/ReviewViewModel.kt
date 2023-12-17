d0563195a019e88b063244e15541b5985981247b
WasiliySoft-Home <wasiliysoft@ya.ru>
2023-12-17 12:02:16 +0500

    create coroutine errorHandler in ReviewViewModel

--- app/src/main/java/ru/wasiliysoft/rustoreconsole/screen/reviews/ReviewViewModel.kt
+++ app/src/main/java/ru/wasiliysoft/rustoreconsole/screen/reviews/ReviewViewModel.kt
@@ -5,11 +5,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 import ru.wasiliysoft.rustoreconsole.data.AppInfo
 import ru.wasiliysoft.rustoreconsole.data.UserReview
 import ru.wasiliysoft.rustoreconsole.network.RetrofitClient
@@ -25,6 +27,7 @@ data class Review(
 //всегда есть вариант собрать луковку (клин) и вынести часть логики в юзкейсы
 //тем более они для этого и нужны
 class ReviewViewModel : ViewModel() {
+    private val LOG_TAG = "ReviewViewModel"
     private val api = RetrofitClient.api
     private val appListRepo = AppListRepository
 
@@ -34,47 +37,46 @@ class ReviewViewModel : ViewModel() {
         MutableLiveData<LoadingResult<List<Review>>>(LoadingResult.Loading("Инициализация"))
     val reviews: LiveData<LoadingResult<List<Review>>> = _reviews
 
+    private val errorHandler = CoroutineExceptionHandler { _, exception ->
+        _reviews.postValue(LoadingResult.Error(Exception(exception.message, exception)))
+        Log.e(LOG_TAG, exception.message.toString())
+        exception.printStackTrace()
+    }
+
     init {
-        Log.d("ReviewViewModel", "init")
+        Log.d(LOG_TAG, "init")
         loadReviews()
     }
 
     fun loadReviews() {
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(errorHandler) {
             _reviews.postValue(LoadingResult.Loading("Загружаем..."))
-            val list = mutableListOf<Review>()
-            val exceptionList = mutableListOf<Exception>()
             val appIds = appListRepo.getApps() ?: emptyList()
             if (appIds.isEmpty()) {
                 _reviews.postValue(LoadingResult.Error(Exception("Empty app id list")))
                 return@launch
             }
+            val list = mutableListOf<Review>()
             appIds.chunked(3).forEach { idList ->
                 idList.map { appInfo ->
                     launch {
-                        try {
-                            val url = "https://backapi.rustore.ru/devs/app/${appInfo.appId}/comment"
-                            val reviews =
-                                api.getReviews(url = url).body.reviews.map {
-                                    Review(
-                                        appInfo = appInfo,
-                                        userReview = it
-                                    )
-                                }
-                            mutex.withLock { list.addAll(reviews) }
-                        } catch (e: Exception) {
-                            exceptionList.add(e)
-                            e.printStackTrace()
-                        }
+                        val reviews = loadReviews(appInfo)
+                        mutex.withLock { list.addAll(reviews) }
                     }
                 }.joinAll()
             }
-            if (exceptionList.isNotEmpty()) {
-                _reviews.postValue(LoadingResult.Error(exceptionList.first()))
-            } else {
-                list.sortByDescending { it.userReview.commentId }
-                _reviews.postValue(LoadingResult.Success(list))
-            }
+            list.sortByDescending { it.userReview.commentId }
+            _reviews.postValue(LoadingResult.Success(list))
+        }
+    }
+
+    private suspend fun loadReviews(appInfo: AppInfo): List<Review> = withContext(Dispatchers.IO) {
+        val url = "https://backapi.rustore.ru/devs/app/${appInfo.appId}/comment"
+        return@withContext api.getReviews(url = url).body.reviews.map {
+            Review(
+                appInfo = appInfo,
+                userReview = it
+            )
         }
     }
 }
