e47d8942a9136c3d4dbee53edf7487f4ee9fc374
WasiliySoft-Home <wasiliysoft@ya.ru>
2024-12-10 19:15:08 +0500

    update libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ apply { from("signingConfigs.gradle") }
 
 android {
     namespace = "ru.wasiliysoft.rustoreconsole"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ru.wasiliysoft.rustoreconsole"
@@ -46,7 +46,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
     packaging {
         resources {
@@ -62,16 +62,16 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-    val nav_version = "2.8.3"
+    val nav_version = "2.8.4"
     implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation("androidx.webkit:webkit:1.12.1")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("com.google.android.material:material:1.12.0")
 
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
-    implementation(platform("androidx.compose:compose-bom:2024.10.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.11.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.runtime:runtime-livedata")
 
@@ -81,13 +81,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.10.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.11.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation(kotlin("reflect"))
 
     // appmetrica
-    implementation("io.appmetrica.analytics:analytics:7.2.0")
-
+    implementation("io.appmetrica.analytics:analytics:7.2.1")
 }
