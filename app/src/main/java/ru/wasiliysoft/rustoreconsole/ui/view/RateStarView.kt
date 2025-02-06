8e7e255bf1bbef7a36671da812629b2642e8e9f6
WasiliySoft-Home <wasiliysoft@ya.ru>
2025-02-06 20:58:03 +0500

    rebuild RateStarView

--- app/src/main/java/ru/wasiliysoft/rustoreconsole/ui/view/RateStarView.kt
+++ app/src/main/java/ru/wasiliysoft/rustoreconsole/ui/view/RateStarView.kt
@@ -1,12 +1,15 @@
 package ru.wasiliysoft.rustoreconsole.ui.view
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.twotone.Star
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -15,23 +18,17 @@ import androidx.compose.ui.unit.dp
 fun RateStarView(
     rate: Int,
     modifier: Modifier = Modifier,
-    maxRate: Int = 5,
     height: Dp = 16.dp
 ) {
     val iconModifier = Modifier.height(height)
-    Row(modifier = modifier) {
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
         repeat(rate) {
             Icon(
                 imageVector = Icons.Filled.Star,
                 contentDescription = "", modifier = iconModifier
             )
         }
-        repeat(maxRate - rate) {
-            Icon(
-                imageVector = Icons.TwoTone.Star,
-                contentDescription = "",
-                modifier = iconModifier
-            )
-        }
+        Spacer(Modifier.size(4.dp))
+        Text("($rate)")
     }
 }
