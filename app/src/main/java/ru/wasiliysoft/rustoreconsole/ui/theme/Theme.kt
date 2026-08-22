29797b11b9c01c7d80f704bbd84c57a2759b43a8
WasiliySoft-Home <wasiliysoft@ya.ru>
2026-08-22 13:22:49 +0500

    fix statusBar text color

--- app/src/main/java/ru/wasiliysoft/rustoreconsole/ui/theme/Theme.kt
+++ app/src/main/java/ru/wasiliysoft/rustoreconsole/ui/theme/Theme.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -40,7 +39,7 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun RuStoreConsoleTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
+    // Динамический цвет поддерживается с Android 12+ (API 31)
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -49,16 +48,22 @@ fun RuStoreConsoleTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
+    // НАСТРОЙКА ЦВЕТА ИКОНОК СТАТУС-БАРА ПОД ДИНАМИЧЕСКИЙ ЦВЕТ
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+
+            // Получаем контроллер системных окон
+            val windowInsetsController = WindowCompat.getInsetsController(window, view)
+
+            // Если тема СВЕТЛАЯ (darkTheme == false), то иконки должны быть ТЁМНЫМИ (true)
+            // Если тема ТЁМНАЯ (darkTheme == true), то иконки должны быть СВЕТЛЫМИ (false)
+            windowInsetsController.isAppearanceLightStatusBars = !darkTheme
         }
     }
 
