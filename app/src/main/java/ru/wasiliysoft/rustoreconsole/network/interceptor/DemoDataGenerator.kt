e878e4c4b0f3cb5ac7054719d816797aa26f4b8f
WasiliySoft-Home <wasiliysoft@ya.ru>
2026-09-26 19:01:22 +0500

    fix basePriceKopecks in DemoDataGenerator.kt

--- app/src/main/java/ru/wasiliysoft/rustoreconsole/network/interceptor/DemoDataGenerator.kt
+++ app/src/main/java/ru/wasiliysoft/rustoreconsole/network/interceptor/DemoDataGenerator.kt
@@ -35,7 +35,7 @@ object DemoDataGenerator {
         val dailyWeight: Double             // вес для "сегодня"
     )
 
-    private val profiles = List(16) {
+    private val profiles = List(6) {
         val index = it + 1
         AppProfile(
             appId = "20$index",
@@ -44,7 +44,7 @@ object DemoDataGenerator {
             shortDescription = "My $index demo app name - app small description",
             companyName = "First name developer",
             companyId = 101,
-            basePriceKopecks = 14900 + index * 10,
+            basePriceKopecks = 14900 + index * 100,
             totalPaidCount = 2813 + index * 10,
             avgPerDay = 2.3,
             dailyWeight = 1.0
